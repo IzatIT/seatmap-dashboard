bb87ce6cc004db701e1cd6164e47f145c8e2fa9a
Izat Tilek uulu <103048933+IzatIT@users.noreply.github.com>
2024-11-30 23:53:45 +0600

    seatmap

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { AppSeatMap } from './components'
+import "@mezh-hq/react-seat-toolkit/styles";
+
+
+function App() {
+
+  return (
+    <>
+      <AppSeatMap />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/types/index.d.ts
@@ -0,0 +1,4 @@
+declare module '@mezh-hq/react-seat-toolkit/styles' {
+    const styles: { [key: string]: string };
+    export default styles;
+}
